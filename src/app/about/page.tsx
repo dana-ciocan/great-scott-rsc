df14214c16d1250a0189713ea09e6cdf1d7b7fce
Dana Ciocan <danaciocan@economist.com>
2024-02-26 18:25:55 +0000

    Set up basic site

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import styles from './page.module.css';
+
+export default function About() {
+  return (
+    <main className={styles.main}>
+      <p>
+        We sell flux capacitors and all kinds of other gadgets and gizmos
+        specifically designed for building reliable time machines.
+      </p>
+      <p>
+        <em>You can trust us to get you when you are going!</em>
+      </p>
+    </main>
+  );
+}
